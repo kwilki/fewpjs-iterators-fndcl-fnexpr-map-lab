77e68d3ef1f3d2b101b69697637f96f2658c5477
Kyle <kylewilkinson14@gmail.com>
2021-11-04 13:09:55 +1100

    finsished tests

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  // use the .map method on the tutorials to return a new array
-  return tutorials
+  return tutorials.map(title => {
+    const words = title.split(" ");
+    const upperCaseWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    const newTitle = upperCaseWords.join(" ");
+    return newTitle;
+  });
 }
+
+
